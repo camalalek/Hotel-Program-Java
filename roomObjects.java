8fb0e4c8587fe93dab28904dbea57293f8646285
Camal Aleksandersen <30876754+camalalek@users.noreply.github.com>
2020-11-04 20:47:12 +0000

    Create roomObjects.java

---
+++ roomObjects.java
@@ -0,0 +1,18 @@
+package hotelprogram;
+
+public class roomObjects {
+    String roomName;
+    
+    public roomObjects(){
+        roomName = "";
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomString) {
+        roomName = roomString;
+    }
+
+}
